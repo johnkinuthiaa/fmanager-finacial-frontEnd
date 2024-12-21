25915db92053b60e56bfe058f4a933667e65d207
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-12-21 17:06:43 +0300

    transactions

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,6 +1,31 @@
+import {useEffect, useState} from "react";
+
 const Transactions =()=>{
+    const[transactionDone,setTransactions] =useState([])
+    useEffect(() => {
+        fetchTransactions()
+    }, []);
+    const fetchTransactions =(async ()=>{
+
+        const userId =1
+        const response = await fetch(`http://localhost:8080/api/v1/transactions/by/userId?userId=${userId}`)
+        const data =await response.json()
+        setTransactions(data.transactions)
+
+    })
     return(
-        <div>Transactions</div>
+        <div>
+            {transactionDone.map((trans)=>(
+                <div style={{display:"flex",gap:"20px",padding:"10px"}}>
+                    <p>{trans.transactionType}</p>
+                    <p>{trans.senderId}</p>
+                    <p>{trans.receiverId}</p>
+                    <p>{trans.transactionId}</p>
+                    <p>{trans.amount}</p>
+                    <p>{trans.transactionTime}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 export default Transactions
